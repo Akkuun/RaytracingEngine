65b4ddec65dacc41fd4a096d5afa8322b706308b
Akkuun <mathisduban@gmail.com>
2025-11-05 17:45:52 +0100

    writing process optimized + FPS Counter

--- src/ui/RenderWidget.cpp
+++ src/ui/RenderWidget.cpp
@@ -34,19 +34,20 @@ void RenderWidget::renderFrame()
         // Direct access to QImage bits for performance
         uchar *bits = renderedImage.bits();
         const float *data = imageData.data();
-        int pixelCount = width * height;
+        const int pixelCount = width * height;
 
-        //  vectorisable opération
+        //  pragma omp for to parallelize the loop  of writing pixels
+        #pragma omp parallel for
         for (int i = 0; i < pixelCount; ++i)
         {
             int baseIdx = i * 3;
             int baseIdxOut = i * 4; // QImage Format_RGB32 use 4 bytes for each pixel (ARGB)
 
             // Conversion optimized float → byte
-            bits[baseIdxOut + 2] = static_cast<uchar>(std::min(255.0f, data[baseIdx] * 255.0f));     // R
-            bits[baseIdxOut + 1] = static_cast<uchar>(std::min(255.0f, data[baseIdx + 1] * 255.0f)); // G
-            bits[baseIdxOut + 0] = static_cast<uchar>(std::min(255.0f, data[baseIdx + 2] * 255.0f)); // B
-            bits[baseIdxOut + 3] = 255;                                                              // Alpha 
+            bits[baseIdxOut + 2] = data[baseIdx] * 255.0f;     // R
+            bits[baseIdxOut + 1] = data[baseIdx + 1] * 255.0f; // G
+            bits[baseIdxOut + 0] = data[baseIdx + 2] * 255.0f; // B
+            bits[baseIdxOut + 3] = 255;                        // Alpha
         }
 
         
